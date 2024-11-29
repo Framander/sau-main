50eba16dfdff9e1f9aaebb016c8c8b7e47fd045a
Frank <fraliloia@gmail.com>
2024-11-29 13:32:09 -0400

    New document .env

--- src/redux/apiSlices.ts
+++ src/redux/apiSlices.ts
@@ -1,6 +1,10 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+// `${}`10.100.90.232
+const ADDRESS = import .meta.env.VITE_ADDRESS || "localhost"
+console.log();
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/users', credentials: 'include'});
+
+const baseQuery = fetchBaseQuery({ baseUrl: `http://${ADDRESS}:5000/api/users`, credentials: 'include'});
 
 export const apiSlice = createApi({
     baseQuery,
