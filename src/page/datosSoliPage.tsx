92b42e32c059dc5239f765e3348459f7cb751a9e
Frank <fraliloia@gmail.com>
2024-12-04 12:27:06 -0400

    Props implemented

--- src/page/datosSoliPage.tsx
+++ src/page/datosSoliPage.tsx
@@ -1,65 +1,73 @@
 import { useSelector } from 'react-redux';
 
 import '../css/DatosSolicitudPage.css'
+import { useParams } from 'react-router-dom';
+
+function DataUser() {
+    const { userInfo } = useSelector((state: any | null) => state.auth);
+
+    return (
+        <div className="datos-user">
+            <ul>
+                <li>Nombre:</li>
+                <li>Entidad Federal:</li>
+                <li>Direccion:</li>
+            </ul>
+
+            <ul>
+                <li>{userInfo.name} {userInfo.last_name}</li>
+                <li>{userInfo.entidad_federal}</li>
+                <li>{userInfo.address.slice(0, 32)}...</li>
+            </ul>
+
+            <ul>
+                <li>Cedula:</li>
+                <li>Email:</li>
+                <li>Telefono:</li>
+            </ul>
+
+            <ul>
+                <li>{userInfo.cedula}</li>
+                <li>{userInfo.email}</li>
+                <li>{userInfo.number}</li>
+            </ul>
+
+        </div>
+    )
+}
 
 function DatosSolicitudPage() {
-    const { userInfo } = useSelector( (state: any | null ) => state.auth );
+    const { id } = useParams()
 
+    function Tr() {
+        return (
+            <tr>
+                <td>20../20..</td>
+                <td>Que...</td>
+                <td>Que...</td>
+                <td>Que...</td>
+                <td>Que...</td>
+            </tr>
+        )
+    }
 
+// Años Centro Poblado Entidad Federal Municipio Parroquia
     return (
         <>
             <section className="DatosSolicitudPage"  >
 
-                <div className="datos-user">
-                    <ul>
-                        <li>Nombre:</li>
-                        <li>Entidad Federal:</li>
-                        <li>Direccion:</li>
-                    </ul>
-                    
-                    <ul>
-                        <li>{userInfo.name} {userInfo.last_name}</li>
-                        <li>Distrito Capital</li>
-                        <li>{userInfo.address}</li>
-                    </ul>
-
-                    <ul>
-                        <li>Cedula:</li>
-                        <li>Email:</li>
-                        <li>Telefono:</li>
-                    </ul>
-
-                    <ul>
-                        <li>{userInfo.cedula}</li>
-                        <li>{userInfo.email}</li>
-                        <li>{userInfo.number}</li>
-                    </ul>
-
-                </div>
+                <DataUser />
 
                 <section>
                     <h2>Solicitud</h2>
 
                     <table className="petition-table">
                         <thead>
-                            <tr>
-                                <th>Años</th>
-                                <th>Centro Poblado</th>
-                                <th>Entidad Federal</th>
-                                <th>Municipio</th>
-                                <th>Parroquia</th>
-                            </tr>
+                            <Tr />
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>20../20..</td>
-                                <td>Que...</td>
-                                <td>Que...</td>
-                                <td>Que...</td>
-                                <td>Que...</td>
-                            </tr>
+                            <Tr />
                         </tbody>
-
                     </table>
 
 
@@ -106,7 +114,7 @@ function DatosSolicitudPage() {
 
                     </table>
                 </section>
-                
+
                 <ul>
                     <li>Nro. de Factura: 00</li>
                     <li>Nro. de Referencia Bancaria: 00</li>
